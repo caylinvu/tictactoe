21f4994f4ef3b29d1ecebafee780b25cfe83e1f5
Caylin Vu <caylinvu@gmail.com>
2023-03-23 21:34:46 -0500

    Add winning conditions

--- script.js
+++ script.js
@@ -7,8 +7,41 @@ const gameboard = (() => {
 
     let gameboardInput = [];
 
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    let winner;
+
+    const checkWinner = () => {
+        for (i = 0; i < winningConditions.length; i++) {
+
+            const winCondition = winningConditions[i];
+            let a = gameboardInput[winCondition[0]];
+            let b = gameboardInput[winCondition[1]];
+            let c = gameboardInput[winCondition[2]];
+
+            if ((a == b && b == c) && a) {
+                if (a == 'X') {
+                    console.log('player 1 wins!!!');
+                    winner = 1;
+                } else {
+                    console.log('player 2 wins!!!!!');
+                    winner = 2;
+                }
+            }
+        }
+    }
+
     return {
-        gameboardInput
+        gameboardInput, checkWinner, winner
     };
 
 
@@ -42,7 +75,7 @@ const displayController = (() => {
         if (e.target.textContent) {
             return;
         }
-        
+
         if (turn == 1) {
             player1.takeTurn(e);
             turn = 2;
@@ -50,6 +83,8 @@ const displayController = (() => {
             player2.takeTurn(e);
             turn = 1;
         }
+
+        gameboard.checkWinner();
     }
 
     return {
